799885ed71bdd849fe1587ef8cf7f17d5efa89e1
e-inquirer <sorour.a@gmail.com>
2017-03-16 14:03:28 -0400

    example of POST and GET requests

--- src/main/java/org/launchcode/controllers/HelloController.java
+++ src/main/java/org/launchcode/controllers/HelloController.java
@@ -2,6 +2,8 @@ package org.launchcode.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,6 +19,31 @@ public class HelloController {
     public String index(HttpServletRequest request) {
 
         String name = request.getParameter("name");
+
+        //Null check
+        if (name == null) {
+            name = "World";
+        }
+        return "Hello " + name;
+    }
+
+    @RequestMapping(value = "hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String helloForm() {
+        String html = "<form method='post'>" +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet Me!'>" +
+                "</form>";
+
+        return html;
+    }
+
+    @RequestMapping(value = "hello", method = RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(HttpServletRequest request) {
+
+        String name = request.getParameter("name");
+
         return "Hello " + name;
     }
 
