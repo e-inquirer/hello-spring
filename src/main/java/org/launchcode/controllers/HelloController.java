96a389b0df730108f9a079d0403f8d7d769a3acd
e-inquirer <sorour.a@gmail.com>
2017-03-16 12:39:51 -0400

    example of non-root mapping

--- src/main/java/org/launchcode/controllers/HelloController.java
+++ src/main/java/org/launchcode/controllers/HelloController.java
@@ -15,4 +15,10 @@ public class HelloController {
     public String index() {
         return "Hello World";
     }
+
+    @RequestMapping(value = "goodbye")
+    @ResponseBody
+    public String goodbye() {
+        return "Goodbye";
+    }
 }
